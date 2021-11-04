4f4996b09b1e128e4620fe865597238fdb226f5f
ATS <92697714+ATSxp@users.noreply.github.com>
2021-11-04 11:38:58 -0300

    Update ES.lua

--- ES.lua
+++ ES.lua
@@ -2,7 +2,7 @@
 ents = {
 	
 	new = function(self) -- Create entity
-		local o = {} -- Objects/Entities 
+		local o = {} -- Objetcs/Entities
 		
 		o.type = o.type or "undefined"
 		o.id = o.id or "entity" -- Id
@@ -21,53 +21,58 @@ ents = {
 		function o:draw() -- Methods
 			spr(self.spr,self.x,self.y,self.c,1,self.f,0,self.sx,self.sy)
 		end
-		function o:Coll(type) -- Methods
-		local type = type or "top-down"
-	
-		if type == "top-down" then
+		function o:coll(type) -- Methods
+			local type = type or error(string.upper("\n\n< You forgot to define a type in the collision function >\n"))
 			
-			-- Left/Right/Up/Down
-			if 
-			fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
-			fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0)or 
-			fget(mget((self.x+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)or 
-			fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)then
-				self.vx,self.vy = 0,0
-			end
-		
-		elseif type == "side-scroll" then
-			
-			-- Left/Right
-			if 
-			fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
-			fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0)or 
-			fget(mget((self.x+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)or 
-			fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)then
-				self.vx = 0
-			end
-			
-			-- Down
-			if 
-			fget(mget((self.x)//8,(self.y+(self.h)+self.vy)//8),0)or 
-			fget(mget((self.x+self.w-1)//8,(self.y+(self.h)+self.vy)//8),0)then
-				self.vy = 0
+			if type == "top-down" or type == "side-scroll" then
+				-- it's OK
 			else
-				self.vy = self.vy + 0.2 -- gravity
+			 error("\n\n< WHICH IS \" "..type.." \" ? >\n")
 			end
 			
-			-- Up
-			if 
-			self.vy<0 and 
-			(fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
-			fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0))then
-				self.vy = 0
-			end
+			if type == "top-down" then
+				
+				-- Left/Right/Up/Down
+				if 
+				fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
+				fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0)or 
+				fget(mget((self.x+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)or 
+				fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)then
+					self.vx,self.vy = 0,0
+				end
 			
+			elseif type == "side-scroll" then
+				
+				-- Left/Right
+				if 
+				fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
+				fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0)or 
+				fget(mget((self.x+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)or 
+				fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+(self.h-1)+self.vy)//8),0)then
+					self.vx = 0
+				end
+				
+				-- Down
+				if 
+				fget(mget((self.x)//8,(self.y+(self.h)+self.vy)//8),0)or 
+				fget(mget((self.x+self.w-1)//8,(self.y+(self.h)+self.vy)//8),0)then
+					self.vy = 0
+				else
+					self.vy = self.vy + 0.2 -- gravity
+				end
+				
+				-- Up
+				if 
+				self.vy<0 and 
+				(fget(mget((self.x+self.vx)//8,(self.y+self.vy)//8),0)or 
+				fget(mget((self.x+(self.w-1)+self.vx)//8,(self.y+self.vy)//8),0))then
+					self.vy = 0
+				end
+				
+			end
 		end
-	end
 		
 		setmetatable(self,{__index = o}) -- Added entity
 		return o
 	end
-	
 }
